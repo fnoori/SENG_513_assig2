6e3887db8a7a972489a1282684ea237b418dbb8c
Farzam Noori <fnoori431@gmail.com>
2019-02-06 13:51:21 -0700

    finished

---
+++ main.js
@@ -0,0 +1,48 @@
+const EQUAL = '=';
+const MAX_LEN = 8;
+const TIMES_SYMBOL = 'x';
+const CLEAR_SYMBOL = 'c';
+const ERROR = 'ERROR';
+
+let toEval = [];
+let displayArea = $('#num-display');
+
+buttonClicked = (value) => {
+  if (value !== EQUAL) {
+    if (value === CLEAR_SYMBOL) {
+      if ($('#num-display').text() === ERROR) {
+        toEval = [];
+        $('#num-display').text('');
+      }
+
+      toEval.pop();
+      $('#num-display').text(function (_,txt) {
+        return txt.slice(0, -1);
+      });
+
+      return;
+    }
+
+    if (toEval.length >= MAX_LEN) {
+      return;
+    }
+
+    if (value === TIMES_SYMBOL) {
+      toEval.push('*');
+    } else {
+      toEval.push(value);
+    }
+
+    $('#num-display').append(value);
+  } else {
+    try {
+      let evaluated = eval(toEval.join(''));
+      $('#num-display').text(evaluated);
+
+      toEval = [];
+      toEval = evaluated.toString().split('');
+    } catch (e) {
+      $('#num-display').text(ERROR);
+    }
+  }
+}
